d5ce8daa13e2198cdfe1df032f1cc8bc531ed4a4
GustavLithell <gustavlithell@gmail.com>
2022-01-21 09:05:27 +0100

    test written

---
+++ js.js
@@ -0,0 +1,86 @@
+// --------------------------------------------
+// JS TEST FINAL
+// --------------------------------------------
+
+// 1) Start by linking this js file to your index.html file.  What is the correct
+//    place to insert most JS files?
+
+
+
+// 2) Declare 5 variables each with a different variable type as it's value.
+
+
+
+// 3) Next we will write conditional statements. Write:
+//    a) an if else statement that checks your boolean variable and console logs 
+//       your string variable if true and otherwise console logs false
+//    b) a ternary statement ( ? : ) to check if your number variable is greater 
+//       than 5. Console log true if it is and false if it isn't.
+//    c) a conditional of your choice that finds out if your 'string' 
+//       variable is of type string and your 'number' variable is a number. If both are 
+//       true console.log('all types are correct')
+
+
+
+// 4) Write a function that accepts 3 parameters: 2 number and a name.  The function
+//    should add both numbers and return a string of "{name}'s number is {number}".
+
+
+// 5) Invoke the function and save it's result in a variable, then console log the
+//    variable.
+
+
+// 6) Are all functions hoisted?
+
+
+// 7) In the html file there is an h1 element.  Save the node in a variable and then 
+//    set its text to "{YOUR_NAME}'s exam"
+
+
+// 8) Use JS to set the id of the h1 to "heading-main"
+
+
+// 9) Create a node list that selects every p in our HTML file. Then use the forEach
+//    method to remove the class 'p-empty' and add text that say 'content here'
+
+
+// 10) Write a function that first, randomly generates a number between 1 & 100 and
+//     saves it in a variable called r.  Second, use r (radius) to find the area of a circle.
+//     Third, round the result to 2 decimal spaces and console log the result. 
+
+
+// 11) Use addEventListener to listen for a click and turns the event target's
+//     text into a different color. 
+
+
+// 12) Create a for loop that console logs all the even numbers up to 22 not 
+//     including 0. Write the same loop using do...while 
+
+
+// 13) Create an array with at least 4 values in it (use both at least 1 string
+//     and 1 number).  Then push another value into the array.  
+
+
+// 14) Replace the 2nd index position value with 44.
+
+
+// 15) Filter the array to only contain numbers.
+
+
+// 16) Create an object that describes something you like and contains a string,
+//     a number, an array and an object value inside it.  Lastly, create a method attached
+//     to the object.
+
+
+// 17) Create 2 input fields where the user can search for a song and an artist. Then
+//     add a button.  When the button is clicked, an API fetch call should be made (use 
+//     https://api.lyrics.ovh/v1/artist/title to achieve this. You can read more of
+//     the documentation at https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search).
+
+//     Lastly, make a function that renders a div containing: 
+//          -an h2 with the song name
+//          -a h3 with artist name
+//          -a p with the lyrics of the song the user searched for
+
+
+// 18) compress you project into a zip file and send it to me and you're done :)
